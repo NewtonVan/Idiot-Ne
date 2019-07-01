8c635391b5afd693eefe1325e0d393b4151f5b6b
NewtonVan <48061231+NewtonVan@users.noreply.github.com>
2019-07-01 16:51:41 +0800

    AIIdiotNe Eureka version

---
+++ AIIdiotNe/cmake-build-debug/CMakeFiles/AIIdiotNe.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/git/gitcode/c2019/level1/AIIdiotNe/ACautomaton.c" "D:/git/gitcode/c2019/level1/AIIdiotNe/cmake-build-debug/CMakeFiles/AIIdiotNe.dir/ACautomaton.c.obj"
+  "D:/git/gitcode/c2019/level1/AIIdiotNe/AI.c" "D:/git/gitcode/c2019/level1/AIIdiotNe/cmake-build-debug/CMakeFiles/AIIdiotNe.dir/AI.c.obj"
+  "D:/git/gitcode/c2019/level1/AIIdiotNe/ControlMove.c" "D:/git/gitcode/c2019/level1/AIIdiotNe/cmake-build-debug/CMakeFiles/AIIdiotNe.dir/ControlMove.c.obj"
+  "D:/git/gitcode/c2019/level1/AIIdiotNe/Evaluate.c" "D:/git/gitcode/c2019/level1/AIIdiotNe/cmake-build-debug/CMakeFiles/AIIdiotNe.dir/Evaluate.c.obj"
+  "D:/git/gitcode/c2019/level1/AIIdiotNe/Game.c" "D:/git/gitcode/c2019/level1/AIIdiotNe/cmake-build-debug/CMakeFiles/AIIdiotNe.dir/Game.c.obj"
+  "D:/git/gitcode/c2019/level1/AIIdiotNe/Stack.c" "D:/git/gitcode/c2019/level1/AIIdiotNe/cmake-build-debug/CMakeFiles/AIIdiotNe.dir/Stack.c.obj"
+  "D:/git/gitcode/c2019/level1/AIIdiotNe/main.c" "D:/git/gitcode/c2019/level1/AIIdiotNe/cmake-build-debug/CMakeFiles/AIIdiotNe.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
